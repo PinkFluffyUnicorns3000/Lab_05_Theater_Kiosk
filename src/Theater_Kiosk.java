b2095abcbe68f4cd5685fb96b582b1523cddde6a
benhillerich <hillerbh@email.uc.edu>
2023-06-10 12:21:34 -0400

    Initial commit

---
+++ src/Theater_Kiosk.java
@@ -0,0 +1,27 @@
+/*
+class AgeToWristBand
+    main()
+			output “What is your age?”
+			input num age
+			if age >= 21 then
+			    output “You get a wristband.”
+		end If
+    return
+EndClass
+ */
+import java.util.Scanner;
+public class Theater_Kiosk {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        int age = 0;
+        System.out.print("How old are you? ");
+        if (in.hasNextInt()){
+            age = in.nextInt();
+            in.nextLine();
+        }
+        if(age >= 21){
+            System.out.println("You get a paper wristband.");
+        }
+    }
+}
